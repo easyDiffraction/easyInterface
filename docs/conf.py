3e40df016233de72a10988f0475e41b848eae7c1
Simon Ward <2798086+wardsimon@users.noreply.github.com>
2020-02-17 11:12:04 +0100

    Move docs location

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../easyInterface'))
+sys.path.insert(0, os.path.abspath('../easyInterface'))
 
 
 # -- Project information -----------------------------------------------------
@@ -60,3 +60,5 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+master_doc = 'index'
