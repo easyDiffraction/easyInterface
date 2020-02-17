bac1131ada7b291440d2395000e4998c22060cf2
Simon Ward <2798086+wardsimon@users.noreply.github.com>
2020-02-17 11:22:37 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../easyInterface'))
+sys.path.insert(0, os.path.abspath('easyInterface'))
 
 
 # -- Project information -----------------------------------------------------
