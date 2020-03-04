d79e85251111598e32ddbf9325a73a2b54007ac4
GitHub Action <action@github.com>
2020-03-04 14:36:16 +0000

    Update documentation

---
+++ _downloads/0c832aa35aaed941402b6c88ee8f2f4b/depreciated_QTinterface.py
@@ -0,0 +1,25 @@
+"""
+Creating a QT interface
+=======================
+
+This demonstrates an example of how to load an example and create a QT interface to a cryspy calculator. Information
+about the project is then displayed.
+"""
+
+# import os
+#
+# from easyInterface.Utils.Helpers import getExamplesDir
+# from easyInterface.Diffraction.Calculators import CryspyCalculator
+# from easyInterface.Diffraction.QtInterface import QtCalculatorInterface
+#
+# data_dir = getExamplesDir()
+# main_rcif = os.path.join(data_dir, 'Fe3O4_powder-1d_neutrons-pol_5C1(LLB)', 'main.cif')
+# calculator = CryspyCalculator(main_rcif)
+#
+# interface = QtCalculatorInterface(calculator, None)
+#
+# print(interface.project_dict)
+#
+# print(interface.phasesIds())
+#
+# print(interface.getPhase(interface.phasesIds()[0]))
