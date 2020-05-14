814d83649daf078be2d36b837f759f50061175b9
Simon Ward <2798086+wardsimon@users.noreply.github.com>
2020-05-14 16:52:23 +0200

    Move from phases to samples

--- easyInterface/Diffraction/__init__.py
+++ easyInterface/Diffraction/__init__.py
@@ -2,7 +2,7 @@ from easyInterface.Diffraction.Interface import CalculatorInterface
 
 DEFAULT_FILENAMES = {
     'project': 'project.cif',
-    'phases': 'phases.cif',
+    'phases': 'samples.cif',
     'experiments': 'experiments.cif',
     'calculations': 'calculations.cif'
 }
